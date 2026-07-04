68cda3b3e9d5d01aa1017b02e74e9d5f60485e9a
PxB <psychoxbounty96.info@gmail.com>
2026-07-04 17:42:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const CONFIG = {
   positions: {
     subjectNumber: { x: 1055, y: 118 },
     name: { x: 905, y: 186 },
-    talent1: { x: 880, y: 1000 },
-    talent2: { x: 880, y: 1030 }
+    talent1: { x: 900, y: 980 },
+    talent2: { x: 900, y: 1010 }
   },
 
   text: {
